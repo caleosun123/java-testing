850abdb4ee4b12bf07e3bb1eecb2caec86383779
calebsmart1 <117774571+calebsmart1@users.noreply.github.com>
2024-10-29 20:42:14 -0400

    Create PostServlet.java

---
+++ src/main/java/web/javatest/PostServlet.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package web.javatest;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet for ErrorExampleServlet.
+ * @author caleb
+ */
+@WebServlet(name="ErrorExampleServlet", urlPatterns={"/exception"})
+public class ErrorExampleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getParameter("text");
+        String text = request.getParameter("text");
+        response.setContentType("text/html");
+        response.getWriter().println("<p>" + text + "</p>");
+    }
+}
